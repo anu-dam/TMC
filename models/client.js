5a41eae62e055c25a60d71dda2aa6ac40b8c5c8c
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-19 18:20:06 +1030

    completed models

--- models/client.js
+++ models/client.js
@@ -1,9 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
     var Client = sequelize.define("Client", {
+        //for this instance, this is the name of the school
         name: {
             type: DataTypes.STRING,
             allowNull: false
         },
+        //currently this is hardcoded as "active". In future, can change to inactive, in order to remove from school list
         status: {
             type: DataTypes.STRING,
             allowNull: false
@@ -16,6 +18,8 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        //must be correct address
+        //need to use in google map API to show the school location
         address: {
             type: DataTypes.STRING,
             allowNull: false
--- models/client_task.js
+++ models/client_task.js
@@ -1,18 +1,32 @@
 module.exports = function(sequelize, DataTypes) {
     var ClientTask = sequelize.define("ClientTask", {
+        //from client
+        //Assigned client's id
         ClientId: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            references: {
+                model: 'Client',
+                key: 'id'
+            }
         },
+        //from task table, assigned to the particular table
         TaskId: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            references: {
+                model: 'Task',
+                key: 'id'
+            }
         },
+        //two status, 1. assigned, 2. completed
         status: {
             type: DataTypes.STRING,
             allowNull: false
         }
     });
+
+
 
     return ClientTask;
 }
