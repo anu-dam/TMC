3af090fc55a48dae37cbb5aef825d032a2727abc
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-22 00:38:00 +1030

    added functionality to select the row

--- public/js/viewusers.js
+++ public/js/viewusers.js
@@ -1,21 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var usertable = $("#usertable");
+
     function getUsers() {
-        $.get("/api/viewusers", function(data) {             
-            usertable.DataTable( {                
+        $.get("/api/viewusers", function (data) {
+            console.log("finished api query")
+            usertable.DataTable({
                 data: data,
                 rowId: 'id',
                 "columns": [
                     { "data": "name" },
                     { "data": "email" },
                     { "data": "type" },
-                    { "data": "status" },                    
-                    { "data": "Client.name", "defaultContent": ""}//if data is not avalilable, show empty column
+                    { "data": "status" },
+                    { "data": "Client.name", "defaultContent": "" }//if data is not avalilable, show empty column
                 ]
             });
-        console.log(data);
-        });
-      }
-      getUsers();
 
-} );
+            console.log("finished drawing table");
+
+            var table = $('#usertable').DataTable();
+
+            $('#usertable tbody').on('click', 'tr', function () {
+                console.log(table.row(this).data());
+            });
+        })
+
+    }
+
+
+
+
+    getUsers();
+
+});
