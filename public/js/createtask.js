dadbd02fc7d2e6bfed7892b4dde52bf89de4be00
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2020-02-23 18:03:39 +1030

    create clints and Tasks

---
+++ public/js/createtask.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    // Getting references to our form and inputs
+    var taskForm = $("#submit");
+    var title = $("#title");
+    var description = $("#description");
+    var status = $("#status");
+    var date = $("#date");
+    
+
+    // When the form is submitted, we validate there's an email and password entered
+    taskForm.on("click", function(event) {
+        event.preventDefault();
+        var taskData = {
+            title: title.val().trim(),
+            description: description.val().trim(),
+            status: ("created"),
+            date: ("2020-05-30")
+        };
+
+        console.log(taskData);
+
+        createtask(taskData);
+        
+    });
+
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function createtask(taskData) {
+        $.post("/api/createtask", {
+            title: taskData.title,
+            description: taskData.description,
+            status: taskData.status,
+            date: taskData.date
+        })
+            
+            .then(function() {
+                window.location.replace("/viewtasks");
+                // If there's an error, log the error
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+});
+
+
+
