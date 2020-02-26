a1cec3055748095c707dbe42bef5f39d9ae6d634
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-26 20:33:33 +1030

    added new columns in admin assigned task view and model

--- public/js/viewclienttasks.js
+++ public/js/viewclienttasks.js
@@ -22,6 +22,7 @@ function initialiseTables(data) {
         "columns": [
             { "data": "tasks_title" },
             { "data": "tasks_description" },
+            { "data": "clients_name" },
             { "data": "tasks_completedBy" },
             { "data": "clienttasks_status" }
         ]
@@ -47,6 +48,8 @@ $(document).ready(function () {
         $("#complete").attr('data-status', selectedRow.clienttasks_status);
         $("#tasktitle").text(selectedRow.tasks_title);
         $("#taskdetail").text(selectedRow.tasks_description);
+        $("#taskcustname").text(selectedRow.clients_name);    
+        $("#taskcustaddress").text(selectedRow.clients_address);              
         $("#taskcompletedby").text(selectedRow.tasks_completedBy);
         $("#taskstatus").text(selectedRow.clienttasks_status);
         if ($("#taskstatus").text() != "Assigned") {
