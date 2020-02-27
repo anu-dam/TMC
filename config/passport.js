f93025c78c06d3f1077c02e45dd0eb85340ea9a5
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-27 13:57:07 +1030

    added task highlighting for overdue tasks

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,7 @@ passport.use(new LocalStrategy(
         });
       }
       // If none of the above, return the user
-      console.log(dbUser);
+      // console.log(dbUser);
       return done(null, dbUser);
     });
   }
